552531a2e811c145154242a4e08cdde4f2ca0581
ACacti <shangjinv6@163.com>
2022-04-17 17:34:01 +0800

    C++PrimerPlus

--- C++Primer Plus/a.cpp
+++ C++Primer Plus/a.cpp
@@ -2,32 +2,28 @@
 * @Author: ACacti
 * @Date:   2022-04-15 20:54:43
 * @Last Modified by:   shang
-* @Last Modified time: 2022-04-15 22:43:51
+* @Last Modified time: 2022-04-17 16:51:22
 */
 #include <iostream>
 using namespace std;
 class Base{
-public:
-    int a = 1;
-    static int count;
-    Base(){count++;};
-    Base(int a){
-        this-> a = a;
-        count++;
-    }
-    ~Base(){cout << this << ": Destructed" << endl;}
+    public:
+    void foo(){cout << "Foo in base" << endl;}
+
 };
 
-class Derived: public Base{
+class Derived : public Base{
+public:
+    void foo(){cout << "Foo in Derived" << endl;}
+
 };
 
 int main()
 {
-    Base b1;
-    cout << "b1: " << &b1 << endl;
-    b1 = 10;
-    cout << b1.a << endl;
-    // cout << Base::count <<endl;
+    enum Color{BLUE, RED, BLACK};
+    enum Human{YELLOW};
+    int flag = BLUE;
+    cout << (flag == YELLOW) << endl;
+
     return 0;
 }
-int Base::count = 0;
